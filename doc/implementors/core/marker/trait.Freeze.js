543a6ec0bb8784c25fc852128cdd247a611511d5
Ola Yeku <ooyeku@gmail.com>
2023-06-12 17:33:35 -0500

    add documentation

---
+++ doc/implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"lib":[["impl Freeze for <a class=\"struct\" href=\"lib/struct.DataFrame.html\" title=\"struct lib::DataFrame\">DataFrame</a>",1,["lib::DataFrame"]],["impl Freeze for <a class=\"struct\" href=\"lib/struct.Equation.html\" title=\"struct lib::Equation\">Equation</a>",1,["lib::Equation"]],["impl Freeze for <a class=\"struct\" href=\"lib/struct.ZeroEquation.html\" title=\"struct lib::ZeroEquation\">ZeroEquation</a>",1,["lib::ZeroEquation"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
